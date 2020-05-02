7d90e6401acc0b12a460728da49e2081999bb077
Sasha <sashi.estrella@gmail.com>
2020-05-02 18:59:42 -0300

    Arreglado recibir mensaje New Pokemon

--- broker/broker.c
+++ broker/broker.c
@@ -2,40 +2,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include "estructuras.h"
+#include <commons/collections/list.h>
 
-
-void crearColaNewPokemon(){
-	t_list* listaNew = list_create();
-}
 
 int main(void)
 {
 
 	//iniciar_servidor();
 
-	pthread_t hilo1, hilo2;
-	char *message1 = "Hilo 1";
-	char *message2 = "Hilo 2";
+	pthread_t hiloConexion;
 
-	int iret1, iret2;
+	New_Pokemon = list_create();
+	Localized_Pokemon = list_create();
+	Get_Pokemon = list_create();
+	Appeared_Pokemon = list_create();
+	Catch_Pokemon = list_create();
+	Caught_Pokemon = list_create();
 
-	iret1 = pthread_create(&hilo1, NULL,(void*)&iniciar_servidor,NULL);
-	if(iret1 != 0){
-		printf("Fallo la creacion del hilo: %d",iret1);
+	pthread_create(&hiloConexion, NULL,(void*)&iniciar_servidor,NULL);
+
+
+
+
+
+	pthread_join(hiloConexion, NULL); //Puede cambiarse por Detach
+/*
+	int mensaje;
+
+while(0){
+	switch(mensaje){
+		case NEW_POKEMON:
+			list_add(suscriptoresNewPokemon,&suscriptor);
+			break;
+		case LOCALIZED_POKEMON:
+			list_add(suscriptoresLocalizedPokemon,&suscriptor);
+			break;
+		case GET_POKEMON:
+			list_add(suscriptoresGetPokemon,&suscriptor);
+			break;
+		case APPEARED_POKEMON:
+			list_add(suscriptoresAppearedPokemon,&suscriptor);
+			break;
+		case CATCH_POKEMON:
+			list_add(suscriptoresCatchPokemon,&suscriptor);
+			break;
+		case CAUGHT_POKEMON:
+			list_add(suscriptoresCaughtPokemon,&suscriptor);
+			break;
 	}
-
-	//Falta mandar mensaje a la cola
-
-	//iret2 = pthread_create( &hilo2, NULL, crearColaNewPokemon ,(void*) message2);
-
-	pthread_join( hilo1, NULL);
-	//pthread_join( hilo2, NULL);
+}
 
 
 
-	printf("Hilo 1 devuelve: %d\n",iret1);
-	//printf("Hilo 2 devuelve: %d\n",iret2);
-	exit(0);
+
+	pthread_join(hiloColaNewPokemon, NULL);
+	pthread_join(hiloColaLocalizedPokemon, NULL);
+	pthread_join(hiloColaGetPokemon, NULL);
+	pthread_join(hiloColaAppearedPokemon, NULL);
+	pthread_join(hiloColaCatchPokemon, NULL);
+	pthread_join(hiloColaCaughtPokemon, NULL);
+*/
+	printf("Han finalizado los thread.\n");
+
+	//mostrar como quedaron todas las listas de suscriptores
 
 	return EXIT_SUCCESS;
 }
+
+
